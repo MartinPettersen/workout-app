27a63d307b20ac2f33cff4a772fb93690911b664
MartinPettersen <searchingtheskyfor@gmail.com>
2022-07-21 15:35:47 +0200

    setup no longer needed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,10 +2,45 @@ import React from 'react'
 import { View, Text, StyleSheet, Button } from "react-native";
 import Intro from '../components/Intro';
 import * as firebase from "firebase";
+import { db } from '../firebase'
+import { collection, doc, setDoc } from "firebase/firestore";
 
 const HomeScreen = ({navigation}) => {
   const user = firebase.auth().currentUser
   console.log(user.email);
+
+
+  const userExists = db.collection("userWorkoutCollection").doc(user.email);
+  userExists.get()
+  .then((doc) => {    
+      if(doc.exists) {
+          console.log("User exists");
+        } else {
+            console.log("User does not exists")
+            CreateUserWorkout();
+        }
+    })
+        
+  const CreateUserWorkout = () => {
+
+    const userData = {
+      "email": user.email,
+      "active_workout" : "",
+      "workouts": [
+      ],
+    }
+
+    db.collection("userWorkoutCollection").doc(userData.email).set(
+      userData, { merge: true}
+    )
+    .then(() => {
+        alert("added user workout")
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+  }
+
   return (
     <View style={styles.container}>
         <Intro navigation={navigation}/>
